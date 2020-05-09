7bbc0dc3fe1bc1e1ff59310bf6fc8e5bd4757bb8
ogzincdlp <o.incedalip@gmail.com>
2020-05-09 23:18:46 +0300

    add fundamentals of power consumption optimization

--- utilities.py
+++ utilities.py
@@ -7,3 +7,9 @@ def print_result(problem):
     print('Final values of the variables:')
     for var in problem.variables():
         print(var, '=', value(var))
+
+        
+def print_power_values(problem, p, appliances, time_steps):
+    print("\t".join(['Time'] + ['App' + str(i) for i in appliances]))
+    for t in time_steps:
+        print("\t".join([str(t)] + [str(int(value(p[(i, t)]))) for i in appliances]))
